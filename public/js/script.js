be0e4644d1788ffdd181a94c31873cffb1c359f3
Majid Alavizadeh <majidonline@gmail.com>
2019-01-31 21:54:28 +0330

    put questions and answers

--- public/js/script.js
+++ public/js/script.js
@@ -1,96 +1,100 @@
 // Set up speech recognition API, dependent on browser
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
-var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent =
+  SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
 
-var colors = ['hamburger', 'pizza', 'fries', 'turd sandwich'];
-var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+var colors = ["hamburger", "pizza", "fries", "turd sandwich"];
+var grammar =
+  "#JSGF V1.0; grammar colors; public <color> = " + colors.join(" | ") + " ;";
 var synth = window.speechSynthesis;
+var step = 0;
+
+var questions = [
+  "Hello there, Soda Survey here with your five daily questions. Am I speaking to Sabina?",
+  "Alright then, am I speaking to Peter?",
+  "Oh hey Peter, good to hear from you! It is time for your first question, are you ready?",
+  "On a scale from zero to ten, ten being highest, how likely are you to order a pizza online this month?"
+];
+var answers = [
+  "Hello Google, please talk to Soda Survey. ",
+  "No.",
+  "Yes.",
+  "Yes.",
+  "Hahaha, I am eating a pizza now! TEN!"
+];
 
 var recognition = new SpeechRecognition();
 var speechRecognitionList = new SpeechGrammarList();
 speechRecognitionList.addFromString(grammar, 1);
 recognition.grammars = speechRecognitionList;
-//recognition.continuous = false;
-recognition.lang = 'en-US';
+recognition.continuous = true;
+recognition.lang = "en-US";
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
 document.body.onclick = function() {
   recognition.start();
-  console.log('Ready to receive a color command.');
-}
+  console.log("Ready to receive a color command.");
+};
 
 recognition.onresult = function(event) {
-    // The SpeechRecognitionEvent results property returns a SpeechRecognitionRejsultList object
-    // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-    // It has a getter so it can be accessed like an array
-    // The [last] returns the SpeechRecognitionResult at the last position.
-    // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-    // These also have getters so they can be accessed like arrays.
-    // The [0] returns the SpeechRecognitionAlternative at position 0.
-    // We then return the transcript property of the SpeechRecognitionAlternative object
-    const voiceTranscript = registerVoiceInput(event)
-    const confidence = event.results[0][0].confidence
-    if (voiceTranscript == 'hey soda') {
-        var speech = new SpeechSynthesisUtterance("Hello, this is Soda");
-        synth.speak(speech);        
+  // The SpeechRecognitionEvent results property returns a SpeechRecognitionRejsultList object
+  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+  // It has a getter so it can be accessed like an array
+  // The [last] returns the SpeechRecognitionResult at the last position.
+  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+  // These also have getters so they can be accessed like arrays.
+  // The [0] returns the SpeechRecognitionAlternative at position 0.
+  // We then return the transcript property of the SpeechRecognitionAlternative object
+  const voiceTranscript = registerVoiceInput(event);
+  const confidence = event.results[0][0].confidence;
+  if (confidence < 0.75) {
+    text = "I'm sorry, I don't understand. Can you repeat yourself?";
+  } else {
+    if (answers[step] == voiceTranscript) {
+      text = questions[step];
+      step++;
+    } else {
+      text = "I cannot understand what you said, please answer to question.";
     }
-    if (confidence < 0.75) {
-        var speechCon = new SpeechSynthesisUtterance("I'm sorry, I don't understand. Can you repeat yourself?");       
-        synth.speak(speechCon);
-    }
-    else {
-        var speech = new SpeechSynthesisUtterance("Your answer is: " + voiceTranscript + " . Say yes to confirm your answer");
-        synth.speak(speech);        
-    }
-}
+  }
+
+  var speech = new SpeechSynthesisUtterance(text);
+  synth.speak(speech);
+};
 
 recognition.onspeechend = function() {
   recognition.stop();
-}
+};
 
 recognition.onnomatch = function(event) {
   diagnostic.textContent = "I didn't recognise that answer.";
-}
+};
 
 recognition.onerror = function(event) {
-  diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
-}
-
+  diagnostic.textContent = "Error occurred in recognition: " + event.error;
+};
 
 function registerVoiceInput() {
-    var last = event.results.length - 1;
-    var voiceTranscript = event.results[last][0].transcript;
-    return voiceTranscript
+  var last = event.results.length - 1;
+  var voiceTranscript = event.results[last][0].transcript;
+  return voiceTranscript;
 }
 
 function helloUser() {
-    
-    // bot : 'Hello. Is this Peter speaking?'
-
-    // Peter : 'Yes, this is Peter.'
-
-    // bot : 'Hello Peter. Do you want to start today's survey?'
-
-    // Peter : 'Yes please.'
-
-    // startSurvey()
-
+  // bot : 'Hello. Is this Peter speaking?'
+  // Peter : 'Yes, this is Peter.'
+  // bot : 'Hello Peter. Do you want to start today's survey?'
+  // Peter : 'Yes please.'
+  // startSurvey()
 }
 
 function startSurvey() {
-
-    // get the survey from X
-
-    // Ask the question
-
-    // Register the response OR ask question again
-
-    // Ask something like: are you sure your answer is X
-
-    // If yes: go to next question
-
-    // If no: ask question again
-
+  // get the survey from X
+  // Ask the question
+  // Register the response OR ask question again
+  // Ask something like: are you sure your answer is X
+  // If yes: go to next question
+  // If no: ask question again
 }
