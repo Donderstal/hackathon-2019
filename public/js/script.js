c95a28f75fc12869c156c77739c7f34644fe2b7a
Majid Alavizadeh <majidonline@gmail.com>
2019-01-31 20:48:17 +0330

    add sample speech

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,38 @@
 // Set up speech recognition API, dependent on browser
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
-var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent =
+  SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+var synth = window.speechSynthesis;
 
-var colors = ['hamburger', 'pizza', 'fries', 'turd sandwich'];
-var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+var colors = ["hamburger", "pizza", "fries", "turd sandwich"];
+var grammar =
+  "#JSGF V1.0; grammar colors; public <color> = " + colors.join(" | ") + " ;";
 
 var recognition = new SpeechRecognition();
 var speechRecognitionList = new SpeechGrammarList();
 speechRecognitionList.addFromString(grammar, 1);
 recognition.grammars = speechRecognitionList;
 //recognition.continuous = false;
-recognition.lang = 'en-US';
+recognition.lang = "en-US";
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
-var diagnostic = document.querySelector('#resultDisplay');
-var bg = document.querySelector('html');
-var hints = document.querySelector('.hints');
+var diagnostic = document.querySelector("#resultDisplay");
+var bg = document.querySelector("html");
+var hints = document.querySelector(".hints");
 
-var colorHTML= '';
-colors.forEach(function(v, i, a){
+var colorHTML = "";
+colors.forEach(function(v, i, a) {
   console.log(v, i);
-  colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
+  colorHTML += '<span style="background-color:' + v + ';"> ' + v + " </span>";
 });
 /* hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try '+ colorHTML + '.'; */
 
 document.body.onclick = function() {
   recognition.start();
-  console.log('Ready to receive a color command.');
-}
+  console.log("Ready to receive a color command.");
+};
 
 recognition.onresult = function(event) {
   // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
@@ -40,44 +43,43 @@ recognition.onresult = function(event) {
   // These also have getters so they can be accessed like arrays.
   // The [0] returns the SpeechRecognitionAlternative at position 0.
   // We then return the transcript property of the SpeechRecognitionAlternative object
-
   var last = event.results.length - 1;
   var voiceTranscript = event.results[last][0].transcript;
 
-  bg.style.backgroundColor = voiceTranscript;
-  console.log('Confidence: ' + event.results[0][0].confidence);
-    console.log(voiceTranscript)
-}
+  // bg.style.backgroundColor = voiceTranscript;
+  // console.log("Confidence: " + event.results[0][0].confidence);
+  // console.log(voiceTranscript);
+
+  var speech = new SpeechSynthesisUtterance("You said" + voiceTranscript);
+  synth.speak(speech);
+};
 
 recognition.onspeechend = function() {
   recognition.stop();
-}
+};
 
 recognition.onnomatch = function(event) {
   diagnostic.textContent = "I didn't recognise that color.";
-}
+};
 
 recognition.onerror = function(event) {
-  diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
-}
-
+  diagnostic.textContent = "Error occurred in recognition: " + event.error;
+};
 
 function registerVoiceInput() {
-    // Register input
-
-    // 
+  // Register input
+  //
 }
 
 function helloUser() {
-    // bot : 'Hello. Is this Peter speaking?'
-    // Peter : 'Yes, this is Peter.'
-    // bot : 'Hello Peter. Do you want to start today's survey?'
-    // Peter : 'Yes please.'
-    // startSurvey()
+  // bot : 'Hello. Is this Peter speaking?'
+  // Peter : 'Yes, this is Peter.'
+  // bot : 'Hello Peter. Do you want to start today's survey?'
+  // Peter : 'Yes please.'
+  // startSurvey()
 }
 
 function startSurvey() {
-    // get the survery from X
-
-    // 
+  // get the survery from X
+  //
 }
